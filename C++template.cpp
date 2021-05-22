872bca75539b73b463f81b75e4975e6acd4f9aa7
Abhishek Lal <abhisheklalsxs@gmail.com>
2021-05-23 00:40:34 +0530

    Simple boilerplate for C++

---
+++ C++template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a; i < b; ++i)
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
